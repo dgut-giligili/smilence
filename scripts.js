de3903e881318ef3724b7f4fbb3f1f4777ee726e
KSARK <hcwenyc@hotmail.com>
2016-03-30 18:47:51 +0800

    Added files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+﻿// JavaScript Document
+//@version 1.0.0
+//@writer Ksar
+
+
+document.write("<form action='/Flow/_BuyFlow' data-ajax='true' data-ajax-begin='onbegin' data-ajax-confirm='计费按自然月结算，所购买的流量下月1日会自动清零，你确定要购买吗？' data-ajax-method='Post' data-ajax-mode='replace' data-ajax-success='buySuc' data-ajax-update='#msg' id='form0' method='post' novalidate='novalidate'>    <ul class='myFlow'><li><input type='radio' id='my1' name='myFlow' value='6'>&nbsp;<label for='my1'>花费 5 元，购买第*个流量包，增加可用流量5G。</label></li></ul><p><input type='submit' class='btnSub' value='无限购买'></p></form>");
+
