7b7d89077ca7d131d7f893870da6d1ad77fd8567
Victorique Ko <victoriqueko@users.noreply.github.com>
2016-04-01 13:34:04 +0800

    Merge 2.0 and 2.5 version

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
 ﻿// JavaScript Document
-//@version 1.0.0
-//@writer Ksar
+//@version 1.1.0
+//@writer Ksar, Kzjnet
 
+var flowTable = document.getElementsByClassName("flowTable")[0];
+while (flowTable.rows.length != 2)
+	flowTable.deleteRow(1);
+flowTable.rows[1].cells[1].innerHTML = "无限流量包";
+flowTable.rows[1].cells[4].innerHTML = "<span class=\"green\">可购买</span>";
 
-document.write("<form action='/Flow/_BuyFlow' data-ajax='true' data-ajax-begin='onbegin' data-ajax-confirm='计费按自然月结算，所购买的流量下月1日会自动清零，你确定要购买吗？' data-ajax-method='Post' data-ajax-mode='replace' data-ajax-success='buySuc' data-ajax-update='#msg' id='form0' method='post' novalidate='novalidate'>    <ul class='myFlow'><li><input type='radio' id='my1' name='myFlow' value='6'>&nbsp;<label for='my1'>花费 5 元，购买第*个流量包，增加可用流量5G。</label></li></ul><p><input type='submit' class='btnSub' value='无限购买'></p></form>");
+var myFlow = document.getElementsByClassName("myFlow")[0];
+while (myFlow.getElementsByTagName("li").length != 1)
+	myFlow.getElementsByTagName("li")[myFlow.getElementsByTagName("li").length - 1].remove();
+myFlow.getElementsByTagName("label")[0].innerHTML = myFlow.getElementsByTagName("label")[0].innerHTML.replace(/[0-9]{1,4}\ [A-Z]{2}（(([0-9]\.[0-9])|[0-9])\ [A-Z]{2}）/, flowTable.rows[1].cells[3].innerHTML);
 
+if (Number(document.getElementsByClassName("score")[0].innerHTML.match("[0-9].[0-9]{2}$")) > 2.5)
+	myFlow.getElementsByTagName("input")[0].value = 6;
+else
+	myFlow.getElementsByTagName("input")[0].value = 2;
