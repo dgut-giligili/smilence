154fc908cc2e1efccde764f845beed41c1246569
Victorique Ko <victoriqueko@users.noreply.github.com>
2016-04-01 13:57:35 +0800

    Select Flow by default

--- scripts.js
+++ scripts.js
@@ -12,6 +12,7 @@ var myFlow = document.getElementsByClassName("myFlow")[0];
 while (myFlow.getElementsByTagName("li").length != 1)
 	myFlow.getElementsByTagName("li")[myFlow.getElementsByTagName("li").length - 1].remove();
 myFlow.getElementsByTagName("label")[0].innerHTML = myFlow.getElementsByTagName("label")[0].innerHTML.replace(/[0-9]{1,4}\ [A-Z]{2}（(([0-9]\.[0-9])|[0-9])\ [A-Z]{2}）/, flowTable.rows[1].cells[3].innerHTML);
+myFlow.getElementById("my1").checked = true;
 
 if (Number(document.getElementsByClassName("score")[0].innerHTML.match("[0-9].[0-9]{2}$")) > 2.5)
 	myFlow.getElementsByTagName("input")[0].value = 6;
